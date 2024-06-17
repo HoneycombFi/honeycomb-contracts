7aee05559204a92db80d3b821778252623fa0058
JaredBorders <jaredborders@pm.me>
2024-06-17 19:39:22 -0400

    👷🏻‍♂️ update based vault

--- src/BasedVault.sol
+++ src/BasedVault.sol
@@ -10,72 +10,160 @@ import {ERC4626} from "./tokens/ERC4626.sol";
 contract BasedVault is ERC4626, Owned {
 
     /*//////////////////////////////////////////////////////////////
-                              CONSTRUCTOR
+                                STRUCTS
     //////////////////////////////////////////////////////////////*/
 
-    constructor(
-        address _owner,
-        ERC20 _underlying
-    )
-        Owned(_owner)
-        ERC4626(_underlying, "Based Vault Share", "BVS")
-    {}
-
-    /*//////////////////////////////////////////////////////////////
-                          STRATEGY MANAGEMENT
-    //////////////////////////////////////////////////////////////*/
-
-    event StrategyAdded(bytes32 indexed name);
-    event StrategyRemoved(bytes32 indexed name);
-
-    struct Strategy {
-        bytes32 name;
+    struct Initiate {
         address[] targets;
         bytes[] data;
     }
 
-    /// @notice Record of all strategies by name
-    mapping(bytes32 name => Strategy) public strategies;
-
-    /// @notice Owner adds a new yield bearing strategy
-    /// @param _name The name of the strategy to add
-    /// @param _targets The addresses of the contracts to call
-    /// @param _data The data to pass to the contracts
-    function addStrategy(
-        bytes32 _name,
-        address[] calldata _targets,
-        bytes[] calldata _data
-    )
-        external
-        onlyOwner
-    {
-        strategies[_name] = Strategy(_name, _targets, _data);
-        emit StrategyAdded(_name);
+    struct Sync {
+        address[] targets;
+        bytes[] data;
     }
 
-    /// @notice Owner removes a yield bearing strategy
-    /// @param _name The name of the strategy to remove
-    function removeStrategy(bytes32 _name) external onlyOwner {
-        delete strategies[_name];
-        emit StrategyRemoved(_name);
+    struct Unwind {
+        address[] targets;
+        bytes[] data;
+    }
+
+    struct Strategy {
+        bytes32 name;
+        uint256 genesis;
+        uint256 lastSync;
+        Initiate initiate;
+        Sync sync;
+        Unwind unwind;
     }
 
     /*//////////////////////////////////////////////////////////////
-                                 YEILD
+                                 STATE
     //////////////////////////////////////////////////////////////*/
 
-    /// @custom:todo Implement LP:Strategy yield logic
+    Strategy internal strategy;
+    Strategy internal stagedStrategy;
 
-    /// @notice LP selects yield bearing strategy
-    /// @param _name The name of the strategy to select
-    function selectStrategy(bytes32 _name) external view {
-        /// @custom:todo
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    struct ConstructorParams {
+        address owner;
+        ERC20 underlying;
+        Strategy strategy;
     }
 
-    /// @notice LP unselects yield bearing strategy
-    /// @param _name The name of the strategy to unselect
-    function unselectStrategy(bytes32 _name) external view {
-        /// @custom:todo
+    constructor(ConstructorParams memory _params)
+        Owned(_params.owner)
+        ERC4626(_params.underlying, "Based Vault Share", "BVS")
+    {}
+
+    /*//////////////////////////////////////////////////////////////
+                             INTROSPECTION
+    //////////////////////////////////////////////////////////////*/
+
+    function getStrategy() external view returns (Strategy memory) {
+        return strategy;
+    }
+
+    function getStagedStrategy() external view returns (Strategy memory) {
+        return stagedStrategy;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              INTERACTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    event StrategyInitiated(bytes32 indexed name);
+    event StrategySynced(bytes32 indexed name);
+    event StrategyUnwound(bytes32 indexed name);
+
+    /// @notice Initiate yield bearing strategy
+    function initiate() external onlyOwner {
+        _initiate();
+    }
+
+    function _initiate() internal {
+        _execute({
+            _targets: strategy.initiate.targets,
+            _data: strategy.initiate.data
+        });
+
+        emit StrategyInitiated(strategy.name);
+    }
+
+    /// @notice Sync the strategy to harvest yield
+    /// @dev Can be called by anyone
+    function sync() public {
+        strategy.lastSync = block.timestamp;
+
+        _execute({_targets: strategy.sync.targets, _data: strategy.sync.data});
+
+        emit StrategySynced(strategy.name);
+    }
+
+    /// @notice Unwind the strategy
+    function unwind() external onlyOwner {
+        _unwind();
+    }
+
+    function _unwind() internal {
+        _execute({
+            _targets: strategy.unwind.targets,
+            _data: strategy.unwind.data
+        });
+
+        emit StrategyUnwound(strategy.name);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 UPDATE
+    //////////////////////////////////////////////////////////////*/
+
+    event StrategyStaged(bytes32 indexed name);
+    event StrategyCommitted(bytes32 indexed name);
+
+    error CommitFailed();
+
+    function stage(Strategy memory _strategy) external onlyOwner {
+        _strategy.genesis = block.timestamp;
+        _strategy.lastSync = 0;
+
+        stagedStrategy = _strategy;
+
+        emit StrategyStaged(_strategy.name);
+    }
+
+    function commit() external onlyOwner {
+        require(
+            block.timestamp - stagedStrategy.genesis >= 1 days, CommitFailed()
+        );
+
+        delete stagedStrategy;
+        strategy = stagedStrategy;
+
+        _initiate();
+
+        emit StrategyCommitted(strategy.name);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                EXECUTE
+    //////////////////////////////////////////////////////////////*/
+
+    error ExecutionFailed();
+
+    function _execute(
+        address[] memory _targets,
+        bytes[] memory _data
+    )
+        internal
+    {
+        for (uint256 i = 0; i < _targets.length; i++) {
+            (bool success,) = _targets[i].call(_data[i]);
+            require(success, ExecutionFailed());
+        }
     }
 
 }
