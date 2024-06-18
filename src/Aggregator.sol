7f5e51c0137af24358df83ec8c3dc893eb647582
JaredBorders <jaredborders@pm.me>
2024-06-17 20:39:25 -0400

    👷🏻‍♂️ add aggregator

---
+++ src/Aggregator.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract Aggregator {
+
+    mapping(address account => uint256) public shares;
+    mapping(address account => uint256) public assets;
+
+    /// @custom:todo Implement aggregation logic
+
+}
