eef2bb76b5788f2d9c0ec380c4a823be56b35ac0
JaredBorders <jaredborders@pm.me>
2024-06-17 20:24:36 -0400

    🚀 add configs

---
+++ script/configs/Base.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.26;
+
+contract Base {
+
+    address constant OWNER = address(0xB0D);
+
+    address constant UNDERLYING = address(0xA55E7);
+
+}
---
+++ script/configs/Local.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.26;
+
+import {BasedVault} from "../../src/BasedVault.sol";
+
+contract Local {
+
+    address constant OWNER = address(0xB0D);
+
+    address constant UNDERLYING = address(0xA55E7);
+
+}
