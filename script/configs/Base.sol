1cd9ccd8951f93eda2fbdca2b35ccd5e4a84ffac
JaredBorders <jaredborders@pm.me>
2024-06-25 23:19:19 -0400

    🚀 update configs

--- script/configs/Base.sol
+++ script/configs/Base.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.26;
 
 contract Base {
 
-    address constant UNDERLYING = address(0);
-    address constant SYNTHETIX = address(0);
+    address constant USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+    address constant SYNTHETIX = 0x32C222A9A159782aFD7529c87FA34b96CA72C696;
     address constant PANOPTIC = address(0);
 
 }
--- script/configs/BaseSepolia.sol
+++ script/configs/BaseSepolia.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.26;
 
 contract BaseSepolia {
 
-    address constant UNDERLYING = address(0);
-    address constant SYNTHETIX = address(0);
+    address constant USDC = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+    address constant SYNTHETIX = 0x764F4C95FDA0D6f8114faC54f6709b1B45f919a1;
     address constant PANOPTIC = address(0);
 
 }
