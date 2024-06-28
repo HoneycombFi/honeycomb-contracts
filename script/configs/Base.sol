e6bf9c46e5567973e8f9968fb597319cde8325a0
JaredBorders <jaredborders@pm.me>
2024-06-28 13:30:22 +0200

    🚀 update config

--- script/configs/Base.sol
+++ script/configs/Base.sol
@@ -5,6 +5,8 @@ contract Base {
 
     address constant USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
     address constant SYNTHETIX_CORE = 0x32C222A9A159782aFD7529c87FA34b96CA72C696;
+    address constant SYNTHETIX_SPOT_MARKET =
+        0x18141523403e2595D31b22604AcB8Fc06a4CaA61;
     address constant PANOPTIC = address(0);
 
 }
